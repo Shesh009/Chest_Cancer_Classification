e1689b2e5ff23576ba11151a27e99a96511715f8
Shesh009 <sheshusheshu035@gmail.com>
2024-05-27 16:11:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,31 @@
+# """
+#     This will imports the files as the packages in the local environment.
+# """
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description=f.read()
+
+__version__="0.0.0"
+
+REPO_NAME="MLops_project"
+AUTHOR_USER_NAME="Shesh009"
+SRC_REPO="cnnClassifier"
+AUTHOR_EMAIL="sheshusheshu035@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
